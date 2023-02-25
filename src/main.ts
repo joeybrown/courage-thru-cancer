008f6c5cda75ce895eb3c600b53c0493978eccfc
Joey <joey@binarydogwood.com>
2023-02-25 07:55:58 -0600

    init for givecamp

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
+
 export enum Delays {
   Short = 500,
   Medium = 2000,
   Long = 5000,
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
 function delayedHello(
   name: string,
   delay: number = Delays.Medium,
@@ -23,12 +14,10 @@ function delayedHello(
   );
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
+export async function greeter(name: string): Promise<string> {
   return await delayedHello(name, Delays.Long);
 }
+
+const hey = await greeter('Hello');
+
+console.log(hey);
